6bbc7275bbb351c0bad527a6efa4c4eeb6d8a2b8
Andrey.Torshin <dchorch@gmail.com>
2025-01-16 15:36:40 +0300

    Update configure_VM.sh

--- configure_VM.sh
+++ configure_VM.sh
@@ -1,10 +1,10 @@
 sudo apt update
-sudo apt install -y curl && curl https://get.docker.com -o install.sh && sh install.sh
+sudo snap install docker 
 sudo apt install python3-pip
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-5 # Install cuda-toolkit-12-5
+#wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+#sudo dpkg -i cuda-keyring_1.1-1_all.deb
+#sudo apt-get update
+#sudo apt-get -y install cuda-toolkit-12-5 # Install cuda-toolkit-12-5
 # Install Nvidia Container Toolkit
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
